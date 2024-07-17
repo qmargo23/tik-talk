2b6eb5db59b3f142b06783d0e4c3b1a861788e10
qmargo23 <elen.asplund@gmail.com>
2024-07-17 23:45:18 +0300

    fin

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Profile } from '../../data/interfaces/profile.interface';
-import { ImgUrlPipe } from '../../data/helpers/pipe/img-url.pipe';
+import { ImageUrlPipe } from '../../helpers/pipes/image-url.pipe';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [ImgUrlPipe],
+  imports: [ImageUrlPipe],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
 export class ProfileCardComponent {
   @Input() profile!: Profile;
-  @Input() imgUrl?: ImgUrlPipe;
 }

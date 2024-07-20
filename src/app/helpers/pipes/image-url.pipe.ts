0ee0dffa5cc3ff23d68b91ad498254bf98072eae
qmargo23 <elen.asplund@gmail.com>
2024-07-20 23:35:35 +0300

    profile_init

--- src/app/helpers/pipes/image-url.pipe.ts
+++ src/app/helpers/pipes/image-url.pipe.ts
@@ -2,15 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'imageUrl',
-  standalone: true
+  standalone: true,
 })
 export class ImageUrlPipe implements PipeTransform {
-
-  transform(value : string | null): string| null{
-    if(!value){
+  transform(value: string | null): string | null {
+    if (!value) {
       return null;
-    }
+    } 
     return `https://icherniakov.ru/yt-course/${value}`;
   }
-
 }

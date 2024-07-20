916c2917699b6d81896e00fe39bf64c107e5a5b4
qmargo23 <elen.asplund@gmail.com>
2024-07-21 00:31:42 +0300

    sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page/profile-page.component';
+import { LayoutComponent } from './common-ui/layout/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: SearchPageComponent },
-  { path: 'profile', component: ProfilePageComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: SearchPageComponent },
+      { path: 'profile', component: ProfilePageComponent },
+    ],
+  },
   { path: 'login', component: LoginPageComponent },
 ];
